ae1c15d9cfeee893dc959ceb5b8756dc54bb24e6
Moolibdensplk <mstangel@splunk.com>
2019-02-11 14:49:52 +0000

    adding autosign feature to master, and the agent deployment to nodes

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -52,10 +52,11 @@ tar xvfz puppet-enterprise-2018.1.7-ubuntu-16.04-amd64.tar.gz
 touch pe.conf
 echo '{' >> pe.conf
 echo '"console_admin_password"': '"puppet"' >> pe.conf
+echo '"autosign": true' >> pe.conf
 echo '"puppet_enterprise::puppet_master_host"': '"master.puppet.vm"' >> pe.conf
+echo '"puppet_enterprise::profile::master::code_manager_auto_configure": true' >> pe.conf
 echo '"puppet_enterprise::profile::master::r10k_remote": "git@github.com:moolibdensplk/control_repo.git"' >> pe.conf
 echo '"puppet_enterprise::profile::master::r10k_private_key": "/etc/puppetlabs/puppetserver/ssh/id_rsa"' >> pe.conf
-echo '"puppet_enterprise::profile::master::code_manager_auto_configure": true' >> pe.conf
 echo '}' >> pe.conf
 
 # Run Puppet installation
--- bootstrap_node.sh
+++ bootstrap_node.sh
@@ -1,33 +1,15 @@
 #!/bin/sh
- 
- 
-if ps aux | grep "puppet agent" | grep -v grep 2> /dev/null
-then
-    echo "Puppet Agent is already installed. Moving on..."
-else
-    sudo apt-get install -yq puppet
-fi
- 
-if cat /etc/crontab | grep puppet 2> /dev/null
-then
-    echo "Puppet Agent is already configured. Exiting..."
-else
-    sudo apt-get update -yq && sudo apt-get upgrade -yq
- 
-    sudo puppet resource cron puppet-agent ensure=present user=root minute=30 \
-        command='/usr/bin/puppet agent --onetime --no-daemonize --splay'
- 
-    sudo puppet resource service puppet ensure=running enable=true
- 
-    # Configure /etc/hosts file
-    echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.65.100  master.puppet.vm  master" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.65.101  node1.puppet.vm  node1" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.65.102  node2.puppet.vm  node2" | sudo tee --append /etc/hosts 2> /dev/null
- 
-    # Add agent section to /etc/puppet/puppet.conf
-    echo "" && echo "[agent]\nserver=master.puppet.vm" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
- 
-    sudo puppet agent --enable
-fi
+
+# Configure /etc/hosts file
+echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
+echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
+echo "192.168.65.100  master.puppet.vm  master" | sudo tee --append /etc/hosts 2> /dev/null && \
+echo "192.168.65.101  node1.puppet.vm  node1" | sudo tee --append /etc/hosts 2> /dev/null && \
+echo "192.168.65.102  node2.puppet.vm  node2" | sudo tee --append /etc/hosts 2> /dev/null
+
+# Add agent section to /etc/puppet/puppet.conf
+echo "" && echo "[agent]\nserver=master.puppet.vm" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
+
+#Install PuppetAgent from Puppet-PE Master Server
+curl -k https://master.puppet.vm:8140/packages/current/install.bash | sudo bash
+
