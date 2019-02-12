75efc723c09292f6968e501bcfc2b73b949e3913
Moolibdensplk <mstangel@splunk.com>
2019-02-12 09:55:34 +0000

    bleh

--- manifests/site/profile/manifests/apache/devserver.pp
+++ manifests/site/profile/manifests/apache/devserver.pp
@@ -2,7 +2,7 @@ class profile::apache::devserver {
 
 	apache::vhost {'personal_site':
 	  #Enum['On', 'Off'] $sendfile                       => 'Off',
-	  Enum['On',' Off'] $suphp_engine                   => 'Off',
+	  suphp_engine                   => 'Off',
  	  port => 80,
 		docroot => '/var/www/personal',
 		options => 'Indexes MultiViews',
