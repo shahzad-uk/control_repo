9e98e4550748d3acc99a7ddcb8df50061d5f73e1
Moolibdensplk <mstangel@splunk.com>
2019-02-12 09:26:40 +0000

    solving mystery...

--- manifests/site/role/manifests/apache/devserver.pp
+++ manifests/site/role/manifests/apache/devserver.pp
@@ -1,5 +1,3 @@
 class role::apache::devserver {
-  include profile::base
-  include profile::server
   include profile::apache::devserver
 }
--- manifests/site/role/manifests/jenkins/devmaster.pp
+++ manifests/site/role/manifests/jenkins/devmaster.pp
@@ -1,5 +1,3 @@
 class role::jenkins::devmaster {
-  include profile::base
-  include profile::server
   include profile::jenkins::devmaster
 }
