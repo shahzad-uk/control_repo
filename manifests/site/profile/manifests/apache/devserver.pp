542f213ac8b2fc820f647058e6fe81dbf67d7844
Moolibdensplk <mstangel@splunk.com>
2019-02-12 11:22:37 +0000

    bleh

--- manifests/site/profile/manifests/apache/devserver.pp
+++ manifests/site/profile/manifests/apache/devserver.pp
@@ -1,13 +1,14 @@
 class profile::apache::devserver {
 
-	apache::vhost {'dev_site':
+	apache::vhost {'000-dev_site':
 	  
 	  suphp_engine                   => 'off',
  	  port => 80,
-		docroot => '/var/www/dev',
-		options => 'Indexes MultiViews',
+	  servername => 'dev.puppet.vm',
+	  docroot => '/var/www/dev',
+	  options => 'Indexes MultiViews',
 	}
-    file { '/var/www/production/index.html':
+    file { '/var/www/dev/index.html':
       ensure  => file,
       content => "<H1>Welcome to DEV Website</H1>\n",
     }
--- manifests/site/profile/manifests/apache/prodserver.pp
+++ manifests/site/profile/manifests/apache/prodserver.pp
@@ -1,11 +1,12 @@
 class profile::apache::prodserver {
 
-	apache::vhost {'prod_site':
+	apache::vhost {'000-prod_site':
 	  
 	  suphp_engine                   => 'off',
  	  port => 80,
-		docroot => '/var/www/production',
-		options => 'Indexes MultiViews',
+      servername => 'prod.puppet.vm'
+	  docroot => '/var/www/production',
+	  options => 'Indexes MultiViews',
 	}
     file { '/var/www/production/index.html':
       ensure  => file,
