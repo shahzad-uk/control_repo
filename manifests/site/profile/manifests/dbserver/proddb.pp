f7d37d7d10b1b28fab3cf91c4a708c7354161900
Moolibdensplk <mstangel@splunk.com>
2019-02-12 14:07:49 +0000

    bleh

--- manifests/site/profile/manifests/dbserver/proddb.pp
+++ manifests/site/profile/manifests/dbserver/proddb.pp
@@ -3,8 +3,11 @@ class profile::dbserver::proddb {
   class { '::mysql::server':
     root_password           => 'verystrongpassword',
     remove_default_accounts => true,
-    log_bin => 'ON',
-    bind_address => '0.0.0.0',
+    mysqld => {
+      log_bin => 'ON',
+      bind_address => '0.0.0.0',
+      log-error => '/var/log/mysqld.log',
+    }
   }
   mysql::db {'proddb':
     user     => 'www',
