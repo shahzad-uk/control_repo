de176ef03ea7ed253124756d4da8257352ad6d28
Moolibdensplk <mstangel@splunk.com>
2019-02-12 14:12:56 +0000

    more fun

--- manifests/site/profile/manifests/dbserver/devdb.pp
+++ manifests/site/profile/manifests/dbserver/devdb.pp
@@ -3,11 +3,15 @@ class profile::dbserver::devdb {
   class { '::mysql::server':
     root_password           => 'verystrongpassword',
     remove_default_accounts => true,
-    mysqld => {
-      log_bin => 'ON',
-      bind_address => '0.0.0.0',
-      log-error => '/var/log/mysqld.log',
+    override_options => $override_options
+  }
+  $override_options = {
+    'mysqld' => {
+      'log_bin' => 'ON',
+      'bind_address' => '0.0.0.0',
+      'log-error' => '/var/log/mysqld.log',
     }
+  }
   }
   mysql::db {'devdb':
     user     => 'developer',
--- manifests/site/profile/manifests/dbserver/proddb.pp
+++ manifests/site/profile/manifests/dbserver/proddb.pp
@@ -3,10 +3,13 @@ class profile::dbserver::proddb {
   class { '::mysql::server':
     root_password           => 'verystrongpassword',
     remove_default_accounts => true,
-    mysqld => {
-      log_bin => 'ON',
-      bind_address => '0.0.0.0',
-      log-error => '/var/log/mysqld.log',
+    override_options => $override_options
+  }
+  $override_options = {
+    'mysqld' => {
+      'log_bin' => 'ON',
+      'bind_address' => '0.0.0.0',
+      'log-error' => '/var/log/mysqld.log',
     }
   }
   mysql::db {'proddb':
