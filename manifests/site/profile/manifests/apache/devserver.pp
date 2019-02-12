729d5ce515ddbd7ef67cc938492a3ab4b7b3426e
Moolibdensplk <mstangel@splunk.com>
2019-02-12 10:26:04 +0000

    bleh

--- manifests/site/profile/manifests/apache/devserver.pp
+++ manifests/site/profile/manifests/apache/devserver.pp
@@ -1,16 +1,5 @@
 class profile::apache::devserver {
-    $apache = $::osfamily ? {
-		'RedHat' => 'httpd',
-		'Debian' => 'apache2',
-	}
-	service { $apache:
-	  enable => true,
-	  ensure => true,	  
-	}
-	package { $apache:
-	  ensure => 'installed',
-	}
-	
+
 	apache::vhost {'personal_site':
 	  #Enum['On', 'Off'] $sendfile                       => 'Off',
 	  suphp_engine                   => 'off',
