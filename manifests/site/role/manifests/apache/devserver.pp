d031b3de1693fab6666defd15eb5e893df4c0ce0
Moolibdensplk <mstangel@splunk.com>
2019-02-12 09:59:56 +0000

    bleh

--- manifests/site/role/manifests/apache/devserver.pp
+++ manifests/site/role/manifests/apache/devserver.pp
@@ -1,3 +1,4 @@
 class role::apache::devserver {
+  include profile::apache::base
   include profile::apache::devserver
 }
--- manifests/site/role/manifests/jenkins/devmaster.pp
+++ manifests/site/role/manifests/jenkins/devmaster.pp
@@ -1,3 +1,4 @@
 class role::jenkins::devmaster {
+  include profile::jenkins::base
   include profile::jenkins::devmaster
 }
