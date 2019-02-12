00f6b1f6ae20e8509e737bd65fe2c621d5273961
Moolibdensplk <mstangel@splunk.com>
2019-02-12 13:53:06 +0000

    mysql fun

--- manifests/site/profile/manifests/dbserver/devdb.pp
+++ manifests/site/profile/manifests/dbserver/devdb.pp
@@ -3,18 +3,14 @@ class profile::dbserver::devdb {
   class { '::mysql::server':
     root_password           => 'verystrongpassword',
     remove_default_accounts => true,
-    override_options        => $override_options
+    log_bin => 'ON',
+    bind_address => '0.0.0.0',
   }
-  $override_options = {
-    'mysqld' => {
-      'log_bin' => 'ON',
-      'bind_address' => '0.0.0.0',
-    }
-  } 
-    mysql::db {'devdb':
-        user     => 'developer',
-        password => 'developer',
-        host     => 'localhost',
-        grant    => ['SELECT', 'UPDATE'],
+  mysql::db {'devdb':
+    user     => 'developer',
+    password => 'developer',
+    host     => '*',
+    grant    => ['SELECT', 'UPDATE'],
   }
+
 }
--- manifests/site/profile/manifests/dbserver/proddb.pp
+++ manifests/site/profile/manifests/dbserver/proddb.pp
@@ -3,19 +3,14 @@ class profile::dbserver::proddb {
   class { '::mysql::server':
     root_password           => 'verystrongpassword',
     remove_default_accounts => true,
-    override_options        => $override_options
+    log_bin => 'ON',
+    bind_address => '0.0.0.0',
   }
-  $override_options = {
-    'mysqld' => {
-      'log_bin' => 'ON',
-      'bind_address' => '0.0.0.0',
-    }
-  } 
-    mysql::db {'proddb':
-        user     => 'produser',
-        password => 'produser',
-        host     => '*',
-        grant    => ['SELECT', 'UPDATE'],
-    }
+  mysql::db {'proddb':
+    user     => 'www',
+    password => 'www',
+    host     => '*.puppet.vm',
+    grant    => ['SELECT', 'UPDATE'],
   }
+
 }
