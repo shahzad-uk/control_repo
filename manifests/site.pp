6554cf93faa2294ddd56cd754ae56e884dcf127e
Moolibdensplk <mstangel@splunk.com>
2019-02-12 13:57:53 +0000

    forgot the most important part LOL

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,10 +8,10 @@ node default {
 
 node node1.puppet.vm {
 	include role::apache::devserver
-  #include role::jenkins::devmaster
+  include role::dbserver::devdb
 }
 
 node node2.puppet.vm {
 	include role::apache::prodserver
-  
+  include role::dbserver::proddb
 }
