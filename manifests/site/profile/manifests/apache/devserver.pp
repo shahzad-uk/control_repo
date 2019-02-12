c5766e380c7fac1647e3846e7a447fd2c0d8ca21
Moolibdensplk <mstangel@splunk.com>
2019-02-12 09:53:14 +0000

    more fixes... but progress...

--- manifests/site/profile/manifests/apache/devserver.pp
+++ manifests/site/profile/manifests/apache/devserver.pp
@@ -1,7 +1,9 @@
 class profile::apache::devserver {
 
 	apache::vhost {'personal_site':
-	  port => 80,
+	  #Enum['On', 'Off'] $sendfile                       => 'Off',
+	  Enum['On',' Off'] $suphp_engine                   => 'Off',
+ 	  port => 80,
 		docroot => '/var/www/personal',
 		options => 'Indexes MultiViews',
 	}
